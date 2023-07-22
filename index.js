99b0991c9d8a6f0d098f62a1fc27cf9d462a86aa
ghulam-ahmad-1 <2021ee305@student.uet.edu.pk>
2023-07-22 16:10:38 +0500

    assignment 1

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+// Assigment 01 :  
+//  part 1 :
+// Ive install the VS code , Node js  Completely : 
+// Part 02  :
+var x = "Ahmad ";
+let message = "Hello " + x + "Hope you are fine , lets go outside.";
+console.log("output of part 1 : ");
+console.log(message, "\n");
+// Part 03 : 
+let x1 = "Rajab";
+console.log("Output of part 2 : ", "\n", x1.toLowerCase(), "\n", x1.toUpperCase());
